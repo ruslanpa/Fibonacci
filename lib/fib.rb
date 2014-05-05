0ac358f73b3c5be33e6ddb2d409b8f29f83ce35d
ruslanpa <ruslan.pavlutskiy@gmail.com>
2014-05-05 13:14:48 +0300

    Update fib.rb

--- lib/fib.rb
+++ lib/fib.rb
@@ -1,14 +1,14 @@
 module Fibonacci
 
-	def fibonacci(n = self)
-		if n < 0 or (0..1).include? n
-			n
-		else
-			fibonacci(n - 1) + fibonacci(n - 2)
-		end
-	end
+  def fibonacci(n = self)
+    if n < 0 or (0..1).include? n
+      n
+    else
+      fibonacci(n - 1) + fibonacci(n - 2)
+    end
+  end
 end
 
 class Fixnum
-	include Fibonacci
+  include Fibonacci
 end
